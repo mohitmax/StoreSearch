e32cbd2c1b6c640f501e96697292e9be98908ef2
Mohit Sadhu <msadhu@msadhu.local>
2014-07-16 17:56:37 -0400

    replaced inbuilt storyboard with SearchViewController nib.

---
+++ StoreSearch/SearchViewController.h
@@ -0,0 +1,13 @@
+//
+//  SearchViewController.h
+//  StoreSearch
+//
+//  Created by Mohit Sadhu on 7/16/14.
+//  Copyright (c) 2014 ms. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SearchViewController : UIViewController
+
+@end
