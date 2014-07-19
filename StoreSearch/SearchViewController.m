25b034499986565ea6f00a194d23fe65ba4ac985
mohitmax <mohitmax@gmail.com>
2014-07-19 18:16:33 -0400

    added fake datasource when searching

--- StoreSearch/SearchViewController.m
+++ StoreSearch/SearchViewController.m
@@ -9,10 +9,14 @@
 #import "SearchViewController.h"
 
 @interface SearchViewController ()
-
+@property (nonatomic, weak) IBOutlet UISearchBar* searchBar;
+@property (nonatomic, weak) IBOutlet UITableView* tableView;
 @end
 
 @implementation SearchViewController
+{
+    NSMutableArray* _searchResults;
+}
 
 - (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
 {
@@ -27,12 +31,52 @@
 {
     [super viewDidLoad];
     // Do any additional setup after loading the view from its nib.
+    self.tableView.contentInset = UIEdgeInsetsMake(64, 0, 0, 0);
 }
 
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+#pragma  mark - table view datasource
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    if(_searchResults == nil)
+    {
+        return 0;
+    }
+    else
+    {
+        return _searchResults.count;
+    }
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    static NSString* cellIdentifier = @"SearchResultCell";
+    UITableViewCell* cell = [tableView dequeueReusableCellWithIdentifier:cellIdentifier];
+    
+    if(cell == nil)
+    {
+        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:cellIdentifier];
+    }
+    cell.textLabel.text = _searchResults[indexPath.row];
+    
+    return cell;
+}
+
+#pragma  mark - Search bar delegate
+- (void)searchBarSearchButtonClicked:(UISearchBar *)searchBar
+{
+    _searchResults = [NSMutableArray arrayWithCapacity:10];
+    for (int i = 0; i < 3; i++) {
+        [_searchResults addObject:[NSString stringWithFormat:
+                                   @"Fake Result %d for '%@'", i, searchBar.text]];
+    }
+    [self.tableView reloadData];
 }
 
 @end
